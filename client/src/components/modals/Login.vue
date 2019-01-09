52ef70776ccb0f3013a63b847b9ca3368d657b81
htrammell <hunterttrammell@gmail.com>
2019-01-08 18:19:00 -0600

    made some minor formatting changes

--- client/src/components/modals/Login.vue
+++ client/src/components/modals/Login.vue
@@ -48,11 +48,11 @@ export default {
   width: 300px;
   margin: 0px auto;
   padding: 20px 30px;
-  background-color: #fff;
-  border-radius: 2px;
+  background-color: #859595 !important;
+  border-radius: 25px !important;
   box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
   transition: all .3s ease;
-  font-family: Helvetica, Arial, sans-serif;
+  font-family: roboto, sans-serif;
 }
 
 .modal-header h3 {
