d3b1328c99014b63f0e03ae443e70decda58dd3e
Hunter Trammell <hunterttrammell@gmail.com>
2019-01-10 14:01:03 -0600

    boomerang almost done

---
+++ client/src/components/profile/SideBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="profile-info">
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.profile-info {
+    height: 88vh;
+    width: 300px;
+    background-color: white; 
+    position: fixed;
+    border-top: 10px solid #f39121;
+}
+</style>
