ec88fb58064bf03f457476627abf3efcb25dc9e9
RyanJF <ryanjfreeman77@gmail.com>
2019-01-10 20:35:28 -0600

    working on account.vue page

--- client/src/router.js
+++ client/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import NotFound from './views/NotFound.vue'
 import Profile from './views/Profile.vue'
+import Account from './views/Account.vue'
 
 Vue.use(Router)
 
@@ -24,6 +25,11 @@ export default new Router({
        path: '/profile',
        name: 'profile',
        component: Profile
+     },
+     {
+       path: '/account',
+       name: 'accountManagement',
+       component: Account
      }
   ]
 })
