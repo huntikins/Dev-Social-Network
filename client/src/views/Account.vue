712b8250281377efe1bbf73c8a9982c17c62877b
RyanJF <ryanjfreeman77@gmail.com>
2019-01-10 20:35:34 -0600

    working on account.vue page

---
+++ client/src/views/Account.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <app-boomerang/>
+    <div id="account-management" class="container">
+      <section class="row">
+        <div id="demographic-info" v-if="editDemographics">
+          <p>{{user.firstname + user.lastname}}</p>
+          <p>{{user.email}}</p>
+          <p>{{user.zipcode}}</p>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+
+<script>
+// import API from "./utils";
+import Boomerang from "@/components/profile/Boomerang";
+
+export default {
+  data() {
+    return {
+      editDemographics: false,
+      editInterists: false,
+      editBio: false
+    };
+  },
+  props: ["user"],
+  components: {
+    appBoomerang: Boomerang
+  }
+};
+</script>
+
+<style>
+</style>
