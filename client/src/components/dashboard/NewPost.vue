3b6ac56c0b48d16d200650b54b3fc20119469075
htrammell <hunterttrammell@gmail.com>
2019-01-10 21:29:30 -0600

    created template for dashboard - minimal style

---
+++ client/src/components/dashboard/NewPost.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="new-post-container">
+        <h1 class="">hello world</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.new-post-container {
+    border-bottom: 10px solid #f39121;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+}
+</style>
