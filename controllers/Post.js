28be12782548e7ad98ee2fa6ed98c916dbb5b9f7
David <djknit@gmail.com>
2019-01-16 16:09:31 -0600

    restored controller functions

--- controllers/Post.js
+++ controllers/Post.js
@@ -2,13 +2,61 @@ const Post = require("../models/Post");
 
 module.exports = {
   create: (post, callback) => {
+    post.date = new Date();
     Post.create(post)
       .then(result => callback(result))
       .catch(err => console.error(err));
   },
   delete: (userId, postId, callback) => {
     Post.deleteOne({ user: userId, _id: postId })
-    .then(result => callback(result))
-    .catch(err => console.error(err));
+      .then(result => callback(result))
+      .catch(err => console.error(err));
+  },
+  addComment: (userId, postId, comment, callback) => {
+    Post.updateOne({
+      _id: postId
+    }, {
+      $push: {
+        comments: {
+          body: comment,
+          user: userId,
+          date: new Date()
+        }
+      }
+    }).then(result => callback(result))
+      .catch(err => console.error(err));
+  },
+  deleteComment: (userId, postId, commentId, callback) => {
+    Post.updateOne({
+      _id: postId
+    }, {
+      $pull: {
+        comments: {
+          user: userId,
+          _id: commentId
+        }
+      }
+    }).then(result => callback(result))
+      .catch(err => console.error(err));
+  },
+  like: (userId, postId, callback) => {
+    Post.updateOne({
+      _id: postId
+    }, {
+      $addToSet: {
+        likes: userId
+      }
+    }).then(result => callback(result))
+      .catch(err => console.error(err));
+  },
+  unlike: (userId, postId, callback) => {
+    Post.updateOne({
+      _id: postId
+    }, {
+      $pull: {
+        likes: userId
+      }
+    }).then(result => callback(result))
+      .catch(err => console.error(err));
   }
 }
