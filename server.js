2acb1aa7d29ffeafabd694b6905fbc096215e9b1
htrammell <hunterttrammell@gmail.com>
2019-01-24 19:13:24 -0600

    added prod route for heroku

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const app = require('./config/server').app;
 
 const PORT = process.env.PORT || 8080;
 
+if (process.env.NODE_ENV==="production") {
+  app.use((req, res, next) => {
+    if (req.header['x-forwarded-proto'] !== 'https') {
+      res.redirect('https://' + req.hostname + req.url);
+    } else {
+      next()
+    }
+  })
+}
+
 // Connect to database
 require('./config/database').connect();
 require('./models');
